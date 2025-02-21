50ac31947c142651625de741137256151771a22d
kanu-cast <karkay349@gmail.com>
2025-02-21 13:39:07 +0200

    scaffolding routes

--- types/express.d.ts
+++ types/express.d.ts
@@ -3,6 +3,7 @@ import { Request } from "express";
 declare module "express-serve-static-core" {
   interface Request {
     user?: {
+      photos: any;
       id: string;
       role: "renter" | "host";
       firstName?: string;
